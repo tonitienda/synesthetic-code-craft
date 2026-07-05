e7f1dfe52c98f14c898333130b81bda34a9b124f
Toni Tienda <tonitienda@gmail.com>
2026-07-05 21:00:02 +0200

    Add alive containers project entrypoint

---
+++ src/projects/containers-image-to-running-process-alive.ts
@@ -0,0 +1,8 @@
+import {makeProject} from '@motion-canvas/core';
+
+import aliveStory from '../scenes/containersAliveStory?scene';
+
+export default makeProject({
+  name: 'containers-image-to-running-process-alive',
+  scenes: [aliveStory],
+});
