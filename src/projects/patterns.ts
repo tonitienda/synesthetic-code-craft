6748579bf41cf69dc112ed55e2c7f0747a2f632e
Toni Tienda <tonitienda@gmail.com>
2026-07-06 11:39:39 +0200

    Add reusable patterns Motion Canvas project

---
+++ src/projects/patterns.ts
@@ -0,0 +1,8 @@
+import {makeProject} from '@motion-canvas/core';
+
+import commandPhraseLift from '../scenes/patternCommandPhraseLift?scene';
+
+export default makeProject({
+  name: 'patterns',
+  scenes: [commandPhraseLift],
+});
